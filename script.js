1f25380c43fdcafb67d2240fe6ddec8a1b8a98d5
yashg4509 <bludgermoonstone66@gmail.com>
2024-04-26 13:34:31 -0500

    added spotify q

--- script.js
+++ script.js
@@ -1,3 +1,133 @@
+const topArtistsData = {
+  "items": [
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/3TVXtAsR1Inumwj472S9r4"
+      },
+      "followers": {
+        "href": null,
+        "total": 86844115
+      },
+      "genres": [
+        "canadian hip hop",
+        "canadian pop",
+        "hip hop",
+        "pop rap",
+        "rap"
+      ],
+      "href": "https://api.spotify.com/v1/artists/3TVXtAsR1Inumwj472S9r4",
+      "id": "3TVXtAsR1Inumwj472S9r4",
+      "images": [
+        {
+          "height": 640,
+          "url": "https://i.scdn.co/image/ab6761610000e5eb4293385d324db8558179afd9",
+          "width": 640
+        },
+        {
+          "height": 320,
+          "url": "https://i.scdn.co/image/ab676161000051744293385d324db8558179afd9",
+          "width": 320
+        },
+        {
+          "height": 160,
+          "url": "https://i.scdn.co/image/ab6761610000f1784293385d324db8558179afd9",
+          "width": 160
+        }
+      ],
+      "name": "Drake",
+      "popularity": 93,
+      "type": "artist",
+      "uri": "spotify:artist:3TVXtAsR1Inumwj472S9r4"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/1RyvyyTE3xzB2ZywiAwp0i"
+      },
+      "followers": {
+        "href": null,
+        "total": 16715437
+      },
+      "genres": [
+        "atl hip hop",
+        "hip hop",
+        "rap",
+        "southern hip hop",
+        "trap"
+      ],
+      "href": "https://api.spotify.com/v1/artists/1RyvyyTE3xzB2ZywiAwp0i",
+      "id": "1RyvyyTE3xzB2ZywiAwp0i",
+      "images": [
+        {
+          "height": 640,
+          "url": "https://i.scdn.co/image/ab6761610000e5eb6ef89e23678018c6d8630825",
+          "width": 640
+        },
+        {
+          "height": 320,
+          "url": "https://i.scdn.co/image/ab676161000051746ef89e23678018c6d8630825",
+          "width": 320
+        },
+        {
+          "height": 160,
+          "url": "https://i.scdn.co/image/ab6761610000f1786ef89e23678018c6d8630825",
+          "width": 160
+        }
+      ],
+      "name": "Future",
+      "popularity": 91,
+      "type": "artist",
+      "uri": "spotify:artist:1RyvyyTE3xzB2ZywiAwp0i"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/7rkW85dBwwrJtlHRDkJDAC"
+      },
+      "followers": {
+        "href": null,
+        "total": 3442750
+      },
+      "genres": [
+        "canadian hip hop",
+        "canadian trap",
+        "melodic rap",
+        "rap",
+        "trap"
+      ],
+      "href": "https://api.spotify.com/v1/artists/7rkW85dBwwrJtlHRDkJDAC",
+      "id": "7rkW85dBwwrJtlHRDkJDAC",
+      "images": [
+        {
+          "height": 640,
+          "url": "https://i.scdn.co/image/ab6761610000e5ebf276b6e4b25bb6fb7b942ab2",
+          "width": 640
+        },
+        {
+          "height": 320,
+          "url": "https://i.scdn.co/image/ab67616100005174f276b6e4b25bb6fb7b942ab2",
+          "width": 320
+        },
+        {
+          "height": 160,
+          "url": "https://i.scdn.co/image/ab6761610000f178f276b6e4b25bb6fb7b942ab2",
+          "width": 160
+        }
+      ],
+      "name": "NAV",
+      "popularity": 69,
+      "type": "artist",
+      "uri": "spotify:artist:7rkW85dBwwrJtlHRDkJDAC"
+    }
+  ],
+  "total": 171,
+  "limit": 3,
+  "offset": 0,
+  "href": "https://api.spotify.com/v1/me/top/artists?limit=3&locale=en-US%2Cen%3Bq%3D0.9",
+  "next": "https://api.spotify.com/v1/me/top/artists?offset=3&limit=3&locale=en-US%2Cen%3Bq%3D0.9",
+  "previous": null
+};
+
+
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
@@ -132,4 +262,25 @@ generateMediaLinks();
 function toggleProject(project) {
   project.classList.toggle("expanded");
 }
+
+
+function populateTopArtists() {
+  const artistsContainer = document.getElementById('top-artists-container');
+  artistsContainer.innerHTML = ''; // Clear the container
+  
+  topArtistsData.items.forEach(artist => {
+    const artistElement = document.createElement('div');
+    artistElement.className = 'artist';
+    artistElement.innerHTML = `
+      <a href="${artist.external_urls.spotify}" target="_blank">
+        <img src="${artist.images[0].url}" alt="${artist.name}" class="artist-image">
+      </a>
+      <p class="artist-name"><strong>${artist.name}</strong></p>
+    `;
+    artistsContainer.appendChild(artistElement);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', populateTopArtists);
+
 
